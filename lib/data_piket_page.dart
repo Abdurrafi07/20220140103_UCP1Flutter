da3ca685873f86a543f73ff9443d000d354f6ed6
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-04-29 12:37:09 +0700

    Membuat file data_piket_page dan widget dengan parameter email

---
+++ lib/data_piket_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DataPiketPage extends StatefulWidget {
+  final String email;
+
+  const DataPiketPage({super.key, required this.email});
+
+  @override
+  State<DataPiketPage> createState() => _DataPiketPageState();
+}
+
+class _DataPiketPageState extends State<DataPiketPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
