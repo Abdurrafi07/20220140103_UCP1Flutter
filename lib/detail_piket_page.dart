f9923f28af44e572ac0fb1e8c26c5f94c75f7a36
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-04-29 17:02:15 +0700

    Membuat file detail_piket_page dan widget dengan parameter listTugas

---
+++ lib/detail_piket_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DetailPiketPage extends StatefulWidget {
+  final Map<String, dynamic> listTugas;
+
+  const DetailPiketPage({super.key, required this.listTugas});
+
+  @override
+  State<DetailPiketPage> createState() => _DetailPiketPageState();
+}
+
+class _DetailPiketPageState extends State<DetailPiketPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
