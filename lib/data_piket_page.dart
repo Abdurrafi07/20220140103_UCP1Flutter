8cf46770a1afecf38fdbb49a52e6f3bbfe28ba7b
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-04-29 16:03:09 +0700

    Menambahkan fungsi pemilih tanggal untuk memilih tanggal yang valid

--- lib/data_piket_page.dart
+++ lib/data_piket_page.dart
@@ -13,11 +13,28 @@ class _DataPiketPageState extends State<DataPiketPage> {
   final TextEditingController namaController = TextEditingController();
   final TextEditingController tugasController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
+  DateTime? _selectedDate;
+  bool _isDateInvalid = false;
 
   @override
   void initState() {
     super.initState();
     namaController.text = widget.email;
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: _selectedDate ?? DateTime.now(),
+      firstDate: DateTime(2025),
+      lastDate: DateTime(2045),
+    );
+    if (picked != null) {
+      setState(() {
+        _selectedDate = picked;
+        _isDateInvalid = false;
+      });
+    }
   }
 
   @override
