957da61c5ccdd3e904d2a7a7a77cf619202e42b1
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-04-29 18:24:37 +0700

    Menambahkan input controller pada DataPelangganPage

--- lib/data_pelanggan_page.dart
+++ lib/data_pelanggan_page.dart
@@ -8,6 +8,19 @@ class DataPelangganPage extends StatefulWidget {
 }
 
 class _DataPelangganPageState extends State<DataPelangganPage> {
+  final TextEditingController namaCustController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController noHPController = TextEditingController();
+  final TextEditingController alamatController = TextEditingController();
+  final TextEditingController provinsiController = TextEditingController();
+  final TextEditingController kodePosController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+  
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
