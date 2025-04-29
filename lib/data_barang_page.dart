b996956531956002821faeb6274b8cbcc2509b8e
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-04-29 20:28:32 +0700

    Menambahkan jenis transaksi, jenis barang serta harganya

--- lib/data_barang_page.dart
+++ lib/data_barang_page.dart
@@ -17,6 +17,30 @@ class _DataBarangPageState extends State<DataBarangPage> {
   final TextEditingController hargaSatuanController = TextEditingController();
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
+  int totalHarga = 0;
+
+  List<String> jenisTransaksiList = ['Barang Masuk', 'Barang Keluar'];
+  String? selectedJenisTransaksi;
+
+  List<String> jenisBarangList = [
+    'Pot Bonsai',
+    'Alat Pangkas',
+    'Kawat Bonsai',
+    'Tanah Akadama',
+  ];
+  String? selectedJenisBarang;
+
+  DateTime? _selectedDate;
+  bool _isInvalidDate = false;
+  
+
+  Map<String, int> hargaBarang = {
+    'Pot Bonsai': 30000,
+    'Alat Pangkas': 50000,
+    'Kawat Bonsai': 15000,
+    'Tanah Akadama': 20000,
+  };
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
