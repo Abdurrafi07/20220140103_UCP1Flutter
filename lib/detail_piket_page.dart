e5b304fb3247789fedf637ba9213db1a690bba61
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-04-29 17:09:26 +0700

    Mengubah Stateful menjadi Stateless pada detail_piket_page

--- lib/detail_piket_page.dart
+++ lib/detail_piket_page.dart
@@ -1,15 +1,10 @@
 import 'package:flutter/material.dart';
 
-class DetailPiketPage extends StatefulWidget {
+class DetailPiketPage extends StatelessWidget {
   final Map<String, dynamic> listTugas;
 
   const DetailPiketPage({super.key, required this.listTugas});
 
-  @override
-  State<DetailPiketPage> createState() => _DetailPiketPageState();
-}
-
-class _DetailPiketPageState extends State<DetailPiketPage> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
