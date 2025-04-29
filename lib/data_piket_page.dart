4a0f4e156986237187cf4fac51a234c4f9c69c5d
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-04-29 15:52:43 +0700

    Menambahkan controller teks dan menginisialisasi namaController dengan email

--- lib/data_piket_page.dart
+++ lib/data_piket_page.dart
@@ -10,6 +10,16 @@ class DataPiketPage extends StatefulWidget {
 }
 
 class _DataPiketPageState extends State<DataPiketPage> {
+  final TextEditingController namaController = TextEditingController();
+  final TextEditingController tugasController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    namaController.text = widget.email;
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
