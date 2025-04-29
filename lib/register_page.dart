ccc4eb9998ecde90ff1ab8eb3346e862b50e8b4b
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-04-29 10:09:57 +0700

    Membuat struktur utama register page dengan form dan text

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -26,6 +26,37 @@ class _RegisterPageState extends State<RegisterPage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 50, left: 16, right: 16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/Emblem.png',
+                    width: 100,
+                    height: 100,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 5, bottom: 30),
+                    child: Text(
+                      'Daftar Akun Baru',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
