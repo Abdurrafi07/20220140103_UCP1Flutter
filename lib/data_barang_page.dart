4d94f88ba7b5c7f4704cb9e3fa948063fbbb4c80
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-04-29 20:21:17 +0700

    Membuat controller untuk input pada DataBarangPage

--- lib/data_barang_page.dart
+++ lib/data_barang_page.dart
@@ -8,6 +8,15 @@ class DataBarangPage extends StatefulWidget {
 }
 
 class _DataBarangPageState extends State<DataBarangPage> {
+  final TextEditingController tanggalTransaksiController =
+      TextEditingController();
+  final TextEditingController jenisTransaksiController =
+      TextEditingController();
+  final TextEditingController jenisBarangController = TextEditingController();
+  final TextEditingController jumlahBarangController = TextEditingController();
+  final TextEditingController hargaSatuanController = TextEditingController();
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
