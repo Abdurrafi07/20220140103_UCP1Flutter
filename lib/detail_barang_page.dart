779c6b652d50d676ee0ac28e2b006efd74ec6de9
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-04-29 21:37:42 +0700

    Menambahkan ikon centang dan pesan konfirmasi data berhasil disimpan

--- lib/detail_barang_page.dart
+++ lib/detail_barang_page.dart
@@ -20,6 +20,41 @@ class DetailBarangPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 30),
+              Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color(0xFFDFF5DD),
+                  border: Border.all(
+                    color: Color.fromARGB(255, 76, 27, 140),
+                    width: 2,
+                  ),
+                ),
+                padding: const EdgeInsets.all(20),
+                child: Icon(
+                  Icons.check_circle_rounded,
+                  color: Color.fromARGB(255, 76, 27, 140),
+                  size: 70,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                'Data Berhasil Disimpan',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
