8750ec6d22c75bcef415f217790db89bd1790b84
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-04-29 10:06:29 +0700

    Menambahkan controller input form dan global key untuk validasi

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -8,6 +8,22 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController namaLengkapController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController noHPController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController konfirmasiPasswordController =
+      TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  bool _obscurePassword = true;
+  bool _obscureKonfirmasiPassword = true;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
